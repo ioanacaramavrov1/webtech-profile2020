5858a52c070044ce3d4f56199fa855457473b555
Ioana Caramavrov1 <caramavrovioana17@stud.ase.ro>
2019-10-23 10:28:21 +0000

    Seminar4

--- server.js
+++ server.js
@@ -8,4 +8,21 @@ app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
  
+app.get('/messages', function(request, response){
+    // scriu o functie, ii dau lista de param
+    // fac o vb sa stochez date - un array
+    var messages = [];
+    var message = {
+        //asa fac un obiect in js
+        //obiectele sunt asocieri de tipul cheie si valoare
+        name: "Ioana",
+        message : "Hello from the web"
+    }
+    //ca la stiva, pushez mesajul in array-ul messages
+    messages.push(message);
+    //coedul de status 200 asta inseamna ca e ok ce se intampla
+    response.status(200).send(messages);
+    //pasez obiectul messages 
+    
+})
 app.listen(8080)
